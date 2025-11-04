50b9c3af2b49fd6556bcb65ff35ad1a1d2096a47
MD Abbas <abbas.marvelouscoders@gmail.com>
2025-11-04 12:21:46 +0500

    code

--- src/components/PageBanner.tsx
+++ src/components/PageBanner.tsx
@@ -1,24 +1,20 @@
-import { ChevronRight } from 'lucide-react'
-import Link from 'next/link'
+
 import React from 'react'
 
-const PageBanner = () => {
+// /assets/images/medical.png
+interface Props {
+    bg: string,
+    text: string,
+}
+
+const PageBanner = ({bg, text}: Props) => {
   return (
     <section>
         <div
-            className="fluid-container px-8 bg-[url('/assets/images/medical.png')] 
-                                bg-cover 
-                                bg-center 
-                                h-full 
-                                w-full
-                                relative
-                                "
+            className={`fluid-container px-8 bg-[url('${bg}')] bg-cover bg-center h-full w-full relative`}
             >
             <div className="page-banner-box">
-                <div>
-                    <h1 className="h1">Contact us</h1>
-                    <p>Get in touch with Pharmacy today. We are here to answer your questions</p>
-                </div>
+                <div dangerouslySetInnerHTML={{ __html: text }} />
             </div>
         </div>
     </section>
